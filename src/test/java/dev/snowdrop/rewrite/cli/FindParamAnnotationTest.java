d83219579fbef3c425789a5346aaef1462d9a376
Charles Moulliard <cmoulliard@ibm.com>
2026-02-12 17:21:48 +0100

    Add the shouldFindJakartaGetAnnotation which works on quarkus-resteasy
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- src/test/java/dev/snowdrop/rewrite/cli/FindParamAnnotationTest.java
+++ src/test/java/dev/snowdrop/rewrite/cli/FindParamAnnotationTest.java
@@ -33,10 +33,37 @@ public class FindParamAnnotationTest extends BaseTest {
     }
 
     @Test
-    void shouldFindAnnotation() throws Exception {
+    void shouldFindJakartaGetAnnotation() throws Exception {
         String recipeName = "org.openrewrite.java.search.FindAnnotations";
         String annotationToSearch="jakarta.ws.rs.GET";
-        // org.jboss.resteasy.annotations.jaxrs.PathParam
+
+        // Configure the application to scan and recipe to be executed
+        cfg.setAppPath(Paths.get(appPath));
+        cfg.setFqNameRecipe(recipeName);
+        cfg.setRecipeOptions(Set.of(String.format("annotationPattern=%s",annotationToSearch),"matchMetaAnnotations=false"));
+
+        var results = rewriteCmd.execute(cfg);
+        RecipeRun run = results.getRecipeRuns().get(recipeName);
+
+        assertFalse(run.getDataTables().isEmpty());
+        Optional<Map.Entry<DataTable<?>, List<?>>> resultMap = run.getDataTables().entrySet().stream()
+                .filter(entry -> entry.getKey().getName().contains("SearchResults"))
+                .findFirst();
+        assertTrue(resultMap.isPresent());
+
+        List<?> rows = resultMap.get().getValue();
+        assertEquals(4, rows.size());
+
+        SearchResults.Row record = (SearchResults.Row) rows.getFirst();
+        assertEquals("src/main/java/com/demo/library/BookResource.java", record.getSourcePath());
+        assertEquals("@GET", record.getResult());
+        assertEquals("Find annotations `jakarta.ws.rs.GET`", record.getRecipe());
+    }
+
+    @Test
+    void shouldFindAnnotation() throws Exception {
+        String recipeName = "org.openrewrite.java.search.FindAnnotations";
+        String annotationToSearch="@org.jboss.resteasy.annotations.jaxrs.PathParam"; // That works using : jakarta.ws.rs.GET
 
         // Configure the application to scan and recipe to be executed
         cfg.setAppPath(Paths.get(appPath));

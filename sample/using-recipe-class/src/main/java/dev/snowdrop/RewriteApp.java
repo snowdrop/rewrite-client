e2656d6e09ada00569e749e908f5300ee72ccb17
Charles Moulliard <cmoulliard@ibm.com>
2026-02-23 18:08:46 +0100

    Bump the version from 0.2.9 to 0.2.10 on the README.md, examples, etc
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- sample/using-recipe-class/src/main/java/dev/snowdrop/RewriteApp.java
+++ sample/using-recipe-class/src/main/java/dev/snowdrop/RewriteApp.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS dev.snowdrop.openrewrite:rewrite-client:0.2.9
+//DEPS dev.snowdrop.openrewrite:rewrite-client:0.2.10
 //NOINTEGRATIONS
 //RUNTIME_OPTIONS -Djava.util.logging.manager=org.jboss.logmanager.LogManager
 

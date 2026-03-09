c1a588330d344f85408e8671c3c3c0b13f5dfcf4
Charles Moulliard <cmoulliard@ibm.com>
2026-03-09 17:54:54 +0100

    Added the link to the openrewrite issue for the test case: useRecipesJarToUpgradeDependencies
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- service/src/test/java/dev/snowdrop/rewrite/recipe/tsv/RecipesJarTSVFileTest.java
+++ service/src/test/java/dev/snowdrop/rewrite/recipe/tsv/RecipesJarTSVFileTest.java
@@ -15,6 +15,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @QuarkusTest
 public class RecipesJarTSVFileTest extends BaseTest {
 
+    /*
+       We got the error reported to openrewrite: https://github.com/openrewrite/rewrite-spring-to-quarkus/issues/77
+       java.lang.RuntimeException: Error while visiting src/main/java/com/todo/app/controller/TaskController.java: java.lang.IllegalArgumentException:
+         Unable to find classpath resource dependencies beginning with: 'jakarta.inject-api' in [jakarta.ws.rs-api-3.1.0]
+         org.openrewrite.java.JavaParser.dependenciesFromResources(JavaParser.java:171)
+         org.openrewrite.java.JavaParser$Builder.classpathFromResources(JavaParser.java:318)
+         org.openrewrite.java.ReplaceAnnotation$1.visitAnnotation(ReplaceAnnotation.java:86)
+
+     */
     @Disabled
     @Test
     void useRecipesJarToUpgradeDependencies() throws Exception {
@@ -26,6 +35,7 @@ public class RecipesJarTSVFileTest extends BaseTest {
         cfg.setAppPath(Paths.get(appPath));
         cfg.setFqNameRecipe(recipeName);
         cfg.setAdditionalJarPaths(List.of(
+                "dev.snowdrop.openrewrite:service:jar:shaded:0.2.12-SNAPSHOT",
                 "org.openrewrite.recipe:rewrite-spring-to-quarkus:0.6.0",
                 "org.openrewrite.recipe:rewrite-java-dependencies:1.51.0"
         ));

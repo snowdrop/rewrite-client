c094a02b3cf501b278daa4e6584550d782f8a9bc
Charles Moulliard <cmoulliard@ibm.com>
2026-03-16 10:13:14 +0100

    Bump manually as job didn't replace the new version: 0.3.3-SNAPSHOT
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- sample/using-recipe-class/src/main/java/dev/snowdrop/RewriteApp.java
+++ sample/using-recipe-class/src/main/java/dev/snowdrop/RewriteApp.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS dev.snowdrop.openrewrite:service::shaded
+//DEPS dev.snowdrop.openrewrite:service:0.3.3-SNAPSHOT:shaded
 //NOINTEGRATIONS
 //RUNTIME_OPTIONS -Djava.util.logging.manager=org.jboss.logmanager.LogManager
 
